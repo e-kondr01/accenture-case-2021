e82546a6bbe0863b41b527dc94443e05a67c01ae
e-kondr01 <e.kondr01@gmail.com>
2021-10-22 18:36:54 +0000

    initial commit

---
+++ accenture_case/users/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accenture_case.users"
+    verbose_name = "Пользователи"
