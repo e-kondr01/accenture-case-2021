89f25b3c3e50e8137ec6bfa99b6ca00b637380b3
Egor Kondrashov <e.kondr01@gmail.com>
2021-10-23 15:03:32 +0300

    feat: add month filter

--- accenture_case/kpi_stats/views.py
+++ accenture_case/kpi_stats/views.py
@@ -1,12 +1,29 @@
+import datetime
+
 from rest_framework import generics
 
 from .serializers import *
 
+# Для прототипа
+today = datetime.date(year=2021, month=10, day=6)
+
 
 class KPIEntryListView(generics.ListAPIView):
     serializer_class = KPIEntrySerializer
-    queryset = KPIEntry.objects.all()
-    filterset_fields = ["index", "date"]
+    queryset = KPIEntry.objects.filter(
+        date__gte=today-datetime.timedelta(days=7)
+    )
+    filterset_fields = ["index"]
+
+    def get_queryset(self):
+        if "month" in self.request.query_params:
+            return KPIEntry.objects.filter(
+                date__gte=today-datetime.timedelta(days=30)
+            )
+        else:
+            return KPIEntry.objects.filter(
+                date__gte=today-datetime.timedelta(days=7)
+            )
 
 
 class KPIDashboardView(generics.ListAPIView):
